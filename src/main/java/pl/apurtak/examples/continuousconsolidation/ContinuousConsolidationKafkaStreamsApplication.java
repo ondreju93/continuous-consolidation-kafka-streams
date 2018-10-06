b41796a40df11e2d96e37bad2d7bf0ea4252838c
Andrzej Purtak <apurtak@pl.sii.eu>
2018-10-06 13:33:10 +0200

    Initial commit - spring boot project generated with start.spring.io

---
+++ src/main/java/pl/apurtak/examples/continuousconsolidation/ContinuousConsolidationKafkaStreamsApplication.java
@@ -0,0 +1,12 @@
+package pl.apurtak.examples.continuousconsolidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousConsolidationKafkaStreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousConsolidationKafkaStreamsApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/apurtak/examples/continuousconsolidation/ContinuousConsolidationKafkaStreamsApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.apurtak.examples.continuousconsolidation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ContinuousConsolidationKafkaStreamsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
